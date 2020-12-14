6eaf737cc133b2284207baa2068e5b5d4588e2fd
Ron31 <ron3108@gmx.net>
2020-12-14 23:01:50 +0100

    💩 [forumLiker] timeout

--- Forum-Like/like.user.js
+++ Forum-Like/like.user.js
@@ -9,7 +9,6 @@
 
 (function() {
     'use strict';
-
     var usersLike = [
         12149,
         11704,
@@ -22,17 +21,19 @@
         9694,
         8631
     ];
-    var authorLikeSelector = usersLike.map(user => `.messageAuthor .messageAuthorContainer .userLink[data-user-id="${user}"]`).join(',');
-    var authorHighlightSelector = usersHighlight.map(user => `.messageAuthor .messageAuthorContainer .userLink[data-user-id="${user}"]`).join(',');
-    var highlight = post => post.style.color = 'yellow';
-    var posts = {
-        like: [],
-        highlight: [],
-    };
-    Array.from(document.querySelectorAll('.wbbPost.message')).forEach(post => {
-        if (post.querySelector(authorLikeSelector)) posts.like.push(post);
-        else if (post.querySelector(authorHighlightSelector)) posts.highlight.push(post);
-    });
-    posts.like.forEach(post => post.querySelector('.messageFooterButtons .wcfLikeButton a.button:not(.active)')?.click());
-    posts.highlight.forEach(post => post.querySelector('.messageFooterButtons .wcfLikeButton a.button:not(.active)') && highlight(post));
+    setTimeout(() => {
+        var authorLikeSelector = usersLike.map(user => `.messageAuthor .messageAuthorContainer .userLink[data-user-id="${user}"]`).join(',');
+        var authorHighlightSelector = usersHighlight.map(user => `.messageAuthor .messageAuthorContainer .userLink[data-user-id="${user}"]`).join(',');
+        var highlight = post => post.style.color = 'yellow';
+        var posts = {
+            like: [],
+            highlight: [],
+        };
+        Array.from(document.querySelectorAll('.wbbPost.message')).forEach(post => {
+            if (post.querySelector(authorLikeSelector)) posts.like.push(post);
+            else if (post.querySelector(authorHighlightSelector)) posts.highlight.push(post);
+        });
+        posts.like.forEach(post => post.querySelector('.messageFooterButtons .wcfLikeButton a.button:not(.active)')?.click());
+        posts.highlight.forEach(post => post.querySelector('.messageFooterButtons .wcfLikeButton a.button:not(.active)') && highlight(post));
+    }, 5000)
 })();
