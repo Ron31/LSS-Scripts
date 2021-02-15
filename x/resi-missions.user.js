eaa070a5b3df9277ba6b19ca0f1811d5ef20b98d
Ron31 <ron3108@gmx.net>
2021-02-15 13:39:57 +0100

    ✨ [ReSi] API fix

--- x/resi-missions.user.js
+++ x/resi-missions.user.js
@@ -10,14 +10,15 @@
 
 (async function() {
     'use strict';
-    if(!localStorage.aBuildings || JSON.parse(localStorage.aBuildings).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) await $.getJSON('/api/userDepartments').done(data => localStorage.setItem('aBuildings', JSON.stringify({lastUpdate: new Date().getTime(), value: data})) );
+    if(!localStorage.aBuildings || JSON.parse(localStorage.aBuildings).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) await $.getJSON('/api/userBuildings').done(data => localStorage.setItem('aBuildings', JSON.stringify({lastUpdate: new Date().getTime(), value: data})) );
     const aBuildings = JSON.parse(localStorage.aBuildings).value;
     const f = (x) => Math.ceil(4 * Math.log2(x + 2)+ 0.05 * x) - 4;
+    const dep = aBuildings.filter(x => [1, 3].includes(x.buildingType));
 
     let span = document.querySelector('#missions  span');
     let missions = document.querySelectorAll('div.mission-list-mission');
     let t = document.createElement('span');
-    t.innerHTML = '<span id="Ron31ml">' + missions.length + '</span>' + ' von ' + '<span id="Ron31mlb">' + f(aBuildings.length) + '</span>';
+    t.innerHTML = '<span id="Ron31ml">' + missions.length + '</span>' + ' von ' + '<span id="Ron31mlb">' + f(dep.length) + '</span>';
 
     span.insertAdjacentElement('afterend', t);
 
