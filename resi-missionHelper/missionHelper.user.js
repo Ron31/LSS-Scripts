5d144590a54fb04a6a4a06832804541095fc0b49
Ron31 <ron3108@gmx.net>
2021-06-06 11:10:53 +0200

    ✨ [ReSi] Show Credits

--- resi-missionHelper/missionHelper.user.js
+++ resi-missionHelper/missionHelper.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ReSi-MissionHelper
-// @version      1.1.0
+// @version      1.2.0
 // @description  Einsatzhelfer
 // @author       Ron31
 // @include      https://rettungssimulator.online/mission/*
@@ -33,9 +33,9 @@
     function showPanel(r) {
         let helper = document.createElement('div');
         helper.classList.add('card', 'missionHelper');
-        helper.innerHTML = '<div class="card-headline card-headline-info">Benötigte Mittel</div><div class="card-body"><div class="alert alert-info"><div class="alert-content"><b>Anforderungen können sich durch Variationen ändern.</b></div></div><table id="missionHelper-' + missionID + '"></table></div>';
-        let d = document.querySelector('.alarmed-vehicles');
-        d.insertAdjacentElement('afterbegin', helper);
+        helper.innerHTML = '<div class="card-headline card-headline-info">Benötigte Mittel</div><div class="card-body"><div class="alert alert-info"><div class="alert-content"><b>Anforderungen können sich durch Variationen ändern.</b></div></div><table id="missionHelper-' + missionID + '"></table><div class="alert alert-info"><div class="alert-content"><b>Generelle Informationen:</b></div></div><table id="informationHelper-' + missionID + '"></table></div>';
+        let a = document.querySelector('.alarmed-vehicles');
+        a.insertAdjacentElement('afterbegin', helper);
         let table = document.querySelector('table#missionHelper-' + missionID);
         let tbody = document.createElement('tbody');
         let key, value;
@@ -49,7 +49,27 @@
             tr.appendChild(vehicle)
             tbody.appendChild(tr);
         }
-
         table.appendChild(tbody);
+
+        let table2 = document.querySelector('table#informationHelper-' + missionID);
+        let tbody2 = document.createElement('tbody');
+        let tr = document.createElement('tr');
+        let number = document.createElement('td');
+        number.innerText = r.credits;
+        let vehicle = document.createElement('td');
+        vehicle.innerText = "Credits:";
+        tr.appendChild(vehicle);
+        tr.appendChild(number);
+        tbody2.appendChild(tr);
+        /*let tr2 = document.createElement('tr');
+        let number2 = document.createElement('td');
+        number.innerText = r.fireValue;
+        let vehicle2 = document.createElement('td');
+        vehicle.innerText = "fireValue:";
+        tr2.appendChild(vehicle2);
+        tr2.appendChild(number2);
+        tbody2.appendChild(tr2);*/
+        table2.appendChild(tbody2);
+
     }
 })();
