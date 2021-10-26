1becfa94b8f75efbfe705210a684ccde235e4ecb
Ron31 <ron3108@gmx.net>
2021-10-26 23:11:32 +0200

    ✨ [ReSi] heatmap
    Signed-off-by: Ron31 <ron3108@gmx.net>

---
+++ resi-heatmap.user.js/heatmap.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         ReSi-Heatmap
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  heatmap
+// @author       Ron31
+// @match        https://rettungssimulator.online/
+// @updateURL    https://github.com/Ron31/LSS-Scripts/raw/dev/ReSi-Heatmap/heatmap.user.js
+// @grant        none
+// ==/UserScript==
+(async function() {
+    const ofBuildingTypes = [1,3,5,6];
+    const radius = 4000; //in metres
+    const color = "red"; //CSS Colors & Hex Colors
+    const opacity = 0.05; //Fill opacity.
+
+    Object.keys(departmentMarkers).forEach((buildingMarker) => {
+        if(ofBuildingTypes.includes(document.querySelector(".card[userdepartmentid='" + buildingMarker + "']").getAttribute("buildingtype"))) return;
+        let i = L.circle(departmentMarkers[buildingMarker]._latlng, {
+            color: color,
+            fillColor: color,
+            fillOpacity: opacity,
+            weight: 1,
+            radius: radius
+        })
+        i.addTo(mymap);
+    })
+
+
+
+})();
