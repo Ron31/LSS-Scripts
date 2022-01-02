55a02096012c6acedef1237ed4b138616e7a9e74
Ron31 <ron3108@gmx.net>
2022-01-02 17:30:47 +0100

    ✨ [ReSi-geoJSON] loads geoJSON onto the map
    Signed-off-by: Ron31 <ron3108@gmx.net>

--- resi-geoJSON/resi-geoJSON.user.js
+++ resi-geoJSON/resi-geoJSON.user.js
@@ -5,6 +5,8 @@
 // @author       Ron31
 // @match        https://rettungssimulator.online/*
 // @grant        none
+// @updateURL    https://github.com/Ron31/LSS-Scripts/raw/dev/resi-geoJSON/resi-geoJSON.user.js
+// @downloadURL  https://github.com/Ron31/LSS-Scripts/raw/dev/resi-geoJSON/resi-geoJSON.user.js
 // ==/UserScript==
 
 (async function() {
@@ -15,4 +17,4 @@
         geoData = await (await fetch('https://raw.githubusercontent.com/isellsoap/deutschlandGeoJSON/main/4_kreise/2_hoch.geo.json')).json();
     }
     L.geoJSON(geoData, {style: {fillOpacity: 0.05, color: '#3388ff', weight: 1}}).addTo(mymap);
-    })();
+})();
