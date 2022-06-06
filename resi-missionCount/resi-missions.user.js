3c40c4d1e609669b6980b76e0713681621803b6e
Ron31 <ron3108@gmx.net>
2022-06-06 21:46:29 +0200

    ✨ [ReSi-mH] Update to new Version
    Signed-off-by: Ron31 <ron3108@gmx.net>

--- resi-missionCount/resi-missions.user.js
+++ resi-missionCount/resi-missions.user.js
@@ -9,25 +9,35 @@
 // @grant        none
 // ==/UserScript==
 
+function updateCount() {
+    let ownMissions = document.querySelectorAll('div#missions-container-own div.mission-list-mission');
+    let sharedMissions = document.querySelectorAll('div#missions-container-shared div.mission-list-mission');
+    document.getElementById('missionCount').innerText = ownMissions.length.toString();
+    document.getElementById('missionCountShared').innerText = sharedMissions.length.toString();
+}
+
 (async function() {
     'use strict';
     if(!localStorage.aBuildings || JSON.parse(localStorage.aBuildings).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) await $.getJSON('/api/userBuildings').done(data => localStorage.setItem('aBuildings', JSON.stringify({lastUpdate: new Date().getTime(), value: data})) );
     const aBuildings = JSON.parse(localStorage.aBuildings).value;
     const f = (x) => Math.ceil(4 * Math.log2(x + 2)+ 0.05 * x) - 4;
     let dep = aBuildings.filter(x => GENERATING_BUILDING_IDS.includes(x.buildingType));
+    //document.querySelector('div[tab-id="ownMissions"]').style.width = null;
+    //document.querySelector('div[tab-id="ownMissions"]').style.marginRight = '15px';
+    //document.querySelector('span[tab-id="sharedMissions"]').parentElement.style.width = null;
 
-    let span = document.querySelector('#missions span');
-    let missions = document.querySelectorAll('div.mission-list-mission');
-    let t = document.createElement('span');
-    t.innerHTML = '<span id="missionCount">' + missions.length + '</span>' + ' von ' + '<span id="missionCountPossible">' + f(dep.length) + '</span>';
-
-    span.insertAdjacentElement('afterend', t);
+    let span = document.querySelector('div[tab="ownMissions"]');
+    let span2 = document.querySelector('div[tab="sharedMissions"]');
+    let ownMissions = document.querySelectorAll('div#ownMissions div.missions-container');
+    let sharedMissions = document.querySelectorAll('div#sharedMissions div.missions-container');
+    span.insertAdjacentHTML('afterbegin', '<span class="badge-container"><span class="badge ncOpenMissions" style="color: #fff !important; background-color: red !important;"><span id="missionCount">' + ownMissions.length + '</span>/<span id="missionCountPossible">' + f(dep.length) + '</span></span></span>')
+    span2.insertAdjacentHTML('afterbegin', '<span class="badge-container"><span class="badge ncOpenMissions" style="color: #fff !important; background-color: red !important;" id="missionCountShared">' + sharedMissions.length + '</span></span>')
 
     socket.on("newMission", () => {
-        document.getElementById('missionCount').innerText = parseInt(document.getElementById('missionCount').innerText) + 1;
+        updateCount();
     });
     socket.on("finishMission", () =>{
-        document.getElementById('missionCount').innerText = parseInt(document.getElementById('missionCount').innerText) - 1;
+        updateCount();
     });
     socket.on("departmentBuy", async () => {
         await $.getJSON('/api/userBuildings').done(data => localStorage.setItem('aBuildings', JSON.stringify({lastUpdate: new Date().getTime(), value: data})) );
