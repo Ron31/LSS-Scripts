0d2241f89637958cc29e5f03ee8f3d8a1b437d33
Ron31 <ron3108@gmx.net>
2023-02-22 17:45:48 +0100

    feat: heatmap-new-building
    Signed-off-by: Ron31 <ron3108@gmx.net>

---
+++ resi-heatmap/heatmap-new.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         ReSi-Heatmap-NewBuilding
+// @namespace    http://tampermonkey.net/
+// @version      dev-0.2
+// @description  heatmap for new building
+// @author       Ron31
+// @match        https://rettungssimulator.online/
+// @grant        none
+// ==/UserScript==
+(async function() {
+    const radius = 4000; //in metres
+    const typeToColor = {
+        fireDepartment: "red",
+        fireSchool: "red",
+        emsDepartment: "yellow",
+        emsDoctorDepartment: "yellow",
+        emsSchool: "yellow",
+        lpolDepartment: "blue",
+        bpolDepartment: "blue",
+        policeSchool: "blue",
+        hospital: "green",
+        controlCenter: "white",
+    }
+    const opacity = 0.05; //Fill opacity.
+
+    const origPlaceDepartment = placeDepartment;
+    placeDepartment = async function(pMarker, pType, pTooltip, platlng = false) {
+        await origPlaceDepartment(pMarker, pType, pTooltip, platlng);
+
+
+
+        let i = L.circle(newDepartment._latlng, {
+            color: typeToColor[pType],
+            fillColor: typeToColor[pType],
+            fillOpacity: opacity,
+            weight: 1,
+            radius: radius
+        })
+        i.addTo(mymap);
+        newDepartment.on("dragend", () => {
+            i.setLatLng(newDepartment._latlng);
+        });
+        newDepartment.on("remove", () => {
+            mymap.removeLayer(i);
+        });
+    };
+
+
+
+})();
